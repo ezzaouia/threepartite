4d0799488db6ba210bef21e50c36edfc2ee3ef6d
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2017-02-01 15:08:43 +0100

    first commit threepartite

---
+++ index.js
@@ -0,0 +1,8 @@
+(function () {
+    angular
+        .module('threepartite', [])
+        .component('threepartiteChart', {
+            restricted: 'E',
+            template: '<h2>hello threepartite</h2>'
+        })
+})();
